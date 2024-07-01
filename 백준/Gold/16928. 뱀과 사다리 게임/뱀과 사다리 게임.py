90358dd31f72d93194c8f4e339380c06b6280a20
basisp <144201139+basisp@users.noreply.github.com>
2024-07-01 23:29:11 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 124 ms, Memory: 112004 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -4,42 +4,28 @@ from collections import deque
 S, B = map(int, sys.stdin.readline().split())
 
 visited = [-1]*100
-info = [0 for _ in range(100)]
-
-Q=deque()
+info = list(range(100))
 
 for _ in range(S+B):
     x,y = map(int,sys.stdin.readline().split())
     info[x-1]=(y-1)
 
-Q.append(0)
-
+Q=deque([0])
 visited[0]=0
-
 
 while Q:
     n=Q.popleft()
-    if info[n]!=0:
-        if visited[info[n]]==-1:
-            visited[info[n]]=visited[n]
-            Q.append(info[n])
-        else:
-            visited[info[n]]=min(visited[info[n]],visited[n])
-            Q.append(info[n])
 
-    elif info[n]==0:
-        for dn in range(1,7):
-            tn=n+dn
-            if 0<=tn<=99:
-                if visited[tn]==-1:
-                    visited[tn]=visited[n]+1
-                    Q.append(tn)
-                elif visited[tn]!=-1:
-                    if visited[tn]<=visited[n]+1:
-                        continue
-                    else:
-                        visited[tn]=min(visited[n]+1,visited[tn])
-                        Q.append(tn)
+    if n==99:
+        break
+
+    for dn in range(1,7):
+        tn=n+dn
+        if 0<=tn<=99:
+            final_pos=info[tn]
+            if visited[final_pos]==-1 or visited[final_pos]>visited[n]+1:
+                visited[final_pos]=visited[n]+1
+                Q.append(final_pos)
 
 print(visited[99])
 
