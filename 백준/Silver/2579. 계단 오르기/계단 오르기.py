70bb4ca90a3aba1bb3a9c2ec8977cb6c08f899fc
basisp <144201139+basisp@users.noreply.github.com>
2024-08-15 12:06:53 +0900

    [Silver III] Title: 계단 오르기, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -12,13 +12,9 @@ for _ in range(n):
 dp=[[0]*3 for _ in range(n+1)]
 
 for i in range(1,n+1):
-    for j in range(3):
-        if j==0:
-            dp[i][j]=max(dp[i-1][1],dp[i-1][2])
-        elif j==1:
-            dp[i][j]=dp[i-1][0]+arr[i-1]
-        elif j==2:
-            dp[i][j]=dp[i-1][1]+arr[i-1]
+    dp[i][0]=max(dp[i-1][1],dp[i-1][2])
+    dp[i][1]=dp[i-1][0]+arr[i-1]
+    dp[i][2]=dp[i-1][1]+arr[i-1]
 
 
-print(max(dp[n][1],dp[n][2]))
+print(max(dp[n][1:3]))
