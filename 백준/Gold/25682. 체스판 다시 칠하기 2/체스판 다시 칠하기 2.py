6328bd0e4994e3d634f465ea0162204d86b40ebc
basisp <144201139+basisp@users.noreply.github.com>
2024-08-18 17:48:10 +0900

    [Gold V] Title: 체스판 다시 칠하기 2, Time: 564 ms, Memory: 272080 KB -BaekjoonHub

--- 백준/Gold/25682. 체스판 다시 칠하기 2/체스판 다시 칠하기 2.py
+++ 백준/Gold/25682. 체스판 다시 칠하기 2/체스판 다시 칠하기 2.py
@@ -24,44 +24,30 @@ for i in range(n):
 
 #바꿔줘야 할 부분 1 아닌 부분 0
 
-arr_dpW=[[0]*m for _ in range(n)]
-arr_dpB=[[0]*m for _ in range(n)]
+arr_dpW=[[0]*(m+1) for _ in range(n+1)]
+arr_dpB=[[0]*(m+1) for _ in range(n+1)]
 
 for i in range(n):
     for j in range(m):
         if arr[i][j]!=dpW[i][j]:
-            arr_dpW[i][j]=1
+            arr_dpW[i+1][j+1]=1
         if arr[i][j]!=dpB[i][j]:
-            arr_dpB[i][j]=1
+            arr_dpB[i+1][j+1]=1
 
 #누적합
 
-for i in range(n):
-    for j in range(m):
-        if i==0:
-            arr_dpB[0][j]+=arr_dpB[0][j-1]    
-            arr_dpW[0][j]+=arr_dpW[0][j-1]
-        elif j==0:
-            arr_dpB[i][0]+= arr_dpB[i-1][j]
-            arr_dpW[i][0]+= arr_dpW[i-1][j]
-
-        else:
-            arr_dpB[i][j]+=arr_dpB[i][j-1] + arr_dpB[i-1][j]-arr_dpB[i-1][j-1]
-            arr_dpW[i][j]+=arr_dpW[i][j-1] + arr_dpW[i-1][j]-arr_dpW[i-1][j-1]
+for i in range(1,n+1):
+    for j in range(1,m+1):
+        arr_dpB[i][j]+=arr_dpB[i][j-1] + arr_dpB[i-1][j]-arr_dpB[i-1][j-1]
+        arr_dpW[i][j]+=arr_dpW[i][j-1] + arr_dpW[i-1][j]-arr_dpW[i-1][j-1]
 
 #k*k 에서 누적합의 최소 구하기 (이차원에서의 누적합)
 import math
 min_v=math.inf
-for i in range(k-1,n):
-    for j in range(k-1,m):
-        if i-k>=0 and j-k>=0:
-            min_v=min(min_v, arr_dpB[i][j]-arr_dpB[i][j-k]-arr_dpB[i-k][j]+arr_dpB[i-k][j-k],arr_dpW[i][j]-arr_dpW[i][j-k]-arr_dpW[i-k][j]+arr_dpW[i-k][j-k])
-        elif i-k<0 and j-k<0:
-            min_v=min(min_v, arr_dpW[i][j], arr_dpB[i][j])
-        elif i-k>=0 and j-k<0:
-            min_v=min(min_v, arr_dpB[i][j]-arr_dpB[i-k][j],arr_dpW[i][j]-arr_dpW[i-k][j])
-        elif i-k<0 and j-k>=0:
-            min_v=min(min_v, arr_dpB[i][j]-arr_dpB[i][j-k],arr_dpW[i][j]-arr_dpW[i][j-k])
+for i in range(k,n+1):
+    for j in range(k,m+1):
+        min_v=min(min_v, arr_dpB[i][j]-arr_dpB[i][j-k]-arr_dpB[i-k][j]+arr_dpB[i-k][j-k],arr_dpW[i][j]-arr_dpW[i][j-k]-arr_dpW[i-k][j]+arr_dpW[i-k][j-k])
+
 
 
 print(min_v)
