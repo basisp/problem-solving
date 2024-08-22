5f4bc12a85abf85d992cab3e5f40ef9bbb8c89af
basisp <144201139+basisp@users.noreply.github.com>
2024-08-22 22:24:43 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 132 ms, Memory: 115332 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -1,32 +1,28 @@
 import sys
 from collections import deque
-
-input = sys.stdin.readline
-n, k = map(int, input().split())
-max_v = 100000
-visited = [-1] * (max_v + 1)
-directions = [-1, 1]
-
-def bfs(n):
-    Q = deque([n])
-    visited[n] = 0
-    
+input=sys.stdin.readline
+n,k=map(int,input().split())
+limit=100000
+visited=[-1]*(limit+1)
+direction=[-1,1]
+def dfs(n):
+    Q=deque([n])
+    visited[n]=0
     while Q:
-        num = Q.popleft()
-        
-        # 순간이동 먼저 처리
-        tp = num * 2
-        while tp <= max_v and visited[tp] == -1:
-            visited[tp] = visited[num]
+        num=Q.popleft()
+
+        tp=num*2
+        while tp<=limit and visited[tp]==-1:
+            visited[tp]=visited[num]
             Q.append(tp)
-            tp *= 2
-        
-        # 걷기 처리
-        for dx in directions:
-            nx = num + dx
-            if 0 <= nx <= max_v and visited[nx] == -1:
-                visited[nx] = visited[num] + 1
+            tp*=2
+
+        for dx in direction:
+            nx=num+dx
+            if 0<=nx<=limit and visited[nx]==-1:
+                visited[nx]=visited[num]+1
                 Q.append(nx)
 
-bfs(n)
+dfs(n)
 print(visited[k])
+        
