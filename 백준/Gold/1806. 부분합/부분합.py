e1ac62bf21c8458d62046a41dc4eba1d95a78ae7
basisp <144201139+basisp@users.noreply.github.com>
2024-11-01 23:59:48 +0900

    [Gold IV] Title: 부분합, Time: 116 ms, Memory: 120932 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -1,23 +1,27 @@
 import sys
 
-n, s = map(int, input().split())
-arr = list(map(int, sys.stdin.readline().split()))
+n,s = map(int,input().split())
+arr=list(map(int,sys.stdin.readline().split()))
 
 start = 0
 end = 0
-total = 0
-final_length = float('inf')  # 최소 길이를 저장할 변수 초기화
+total=arr[start]
+final_length = n+1
 
-while end < n:  # end는 n보다 작아야 함
-    total += arr[end]  # 현재 end의 값을 total에 더함
-    end += 1  # end 포인터 증가
+while end < n:
 
-    while total >= s:  # total이 s 이상이면
-        final_length = min(final_length, end - start)  # 길이 업데이트
-        total -= arr[start]  # start의 값을 total에서 빼기
-        start += 1  # start 포인터 증가
+    if total>=s:
+        final_length=min(final_length, end-start+1)
+        total-=arr[start]
+        start+=1
+    else:
+        end+=1
+        if end==n:
+            break
+        total+=arr[end]
 
-if final_length == float('inf'):  # 유효한 부분 수열을 찾지 못한 경우
-    print(0)
-else:
+
+if final_length<n+1:
     print(final_length)
+else:
+    print(0)
